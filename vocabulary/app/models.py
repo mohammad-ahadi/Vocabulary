bc98949a9495e64765fd4f4f5784617f3f18242f
mohammad <admin@example.com>
2019-04-01 22:12:53 +0430

    simple model for word

--- vocabulary/app/models.py
+++ vocabulary/app/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Word(models.Model):
+    english = models.CharField(max_length=255)
+    persian = models.CharField(max_length=255)
+    definition = models.TextField(default='')
+    created_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ('-created_at',)
+
+    def __str__(self):
+        return self.english
