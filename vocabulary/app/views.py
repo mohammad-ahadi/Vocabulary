7ae50c0366925d7c9037a8cc0b87c47165a93baa
mohammad <admin@example.com>
2019-04-01 22:13:42 +0430

    index page url and view

--- vocabulary/app/views.py
+++ vocabulary/app/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Word
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+
+
+def index(request):
+    context = {}
+    words_list = Word.objects.all()
+    paginator = Paginator(words_list, 10)
+    page = request.GET.get('page')
+    try:
+        words = paginator.page(page)
+    except PageNotAnInteger:
+        words = paginator.page(1)
+    except EmptyPage:
+        words = paginator.page(paginator.num_pages)
+    context['words'] = words
+    return render(request, 'app/index.html', context)
